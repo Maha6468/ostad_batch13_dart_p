c3e6a970f231489b8fda0bdb1c50b428ae938839
Maha6468 <motherma7633@gmail.com>
2025-08-24 03:26:30 +0600

    ..

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,15 +1,5 @@
 import 'dart:io';
 void main(){
-  print("Enter your age:");
-  //String? input = stdin.readLineSync();
-  int? age = int.parse(stdin.readLineSync()!);
-  print("Your age is $age");
-
-  double? hight=double.parse(stdin.readLineSync()!);
-  print("My higt: $hight");
-
-  var name=stdin.readLineSync();
-  print("var value is :$name");
 
 
 }
