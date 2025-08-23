1d5bc9948331fdbac916a0c5a880cbd1e6163b3d
Maha6468 <motherma7633@gmail.com>
2025-08-24 02:01:06 +0600

    int null seftey

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,5 +1,16 @@
 import 'dart:io';
 void main(){
+int? age;
+print(age);
+age=25;
+print(age);
 
+int? marks;
+print(marks??0);
+
+int? number;
+print("Is number even? ${number?.isEven}");
+number = 13;
+print("Is number even? ${number?.isEven}");
 
 }
