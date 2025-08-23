ebf3edc803a4e1cc2c0ac0dcbbb1f6eff8dcb13d
Maha6468 <motherma7633@gmail.com>
2025-08-24 02:05:19 +0600

    ..

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,22 +1,4 @@
 import 'dart:io';
 void main(){
-int? age;
-print(age);
-age=25;
-print(age);
-
-int? marks;
-print(marks??0);
-
-int? number;
-print("Is number even? ${number?.isEven}");
-number = 13;
-print("Is number even? ${number?.isEven}");
-
-late int? mark;
-mark=null;
-print(mark);
-mark=15;
-print(mark);
 
 }
