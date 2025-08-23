d26b026b8341c18cd5ba1c60868fe61b84dee6f7
Maha6468 <motherma7633@gmail.com>
2025-08-24 01:44:19 +0600

    ..

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,19 +1,5 @@
 import 'dart:io';
 void main(){
 
-  String? input=null;
-  print(input?.length);
-  input="Mahabubaar";
-  //print(input);
-  print(input?.length);
-  String? nickName = null;
-  print(nickName ?? "No nickname");
-  String? nickNam = "Maha";
-  print(nickNam ?? "No nickname");
-  String? name = "Rahim";
-  print(name!.length);
-
-
-
 
 }
