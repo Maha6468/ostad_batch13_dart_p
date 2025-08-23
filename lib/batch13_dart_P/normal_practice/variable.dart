4aade085dac91b5614cdf4be3672c567a911a55e
Maha6468 <motherma7633@gmail.com>
2025-08-23 21:05:11 +0600

    variable

---
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -0,0 +1,15 @@
+void main(){
+  var name="Mahabubar";
+  int age=12;
+  String country="Bangladesh";
+  double height=56.9;
+  bool isStudent=true;
+  dynamic anything='all variable';
+  final today=DateTime.now();
+  const pi=3.1416;
+
+  print("My name is :$name ,age:$age ,My country is $country ,height:$height");
+  print("It's $isStudent, that Dynamic variable is be anything$anything");
+  print("Time is $today");
+
+}
