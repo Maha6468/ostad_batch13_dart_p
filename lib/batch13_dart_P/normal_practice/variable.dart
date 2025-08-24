a6a9d104ce5389111e847e5d7393f585b307eda2
Maha6468 <motherma7633@gmail.com>
2025-08-24 13:42:30 +0600

    inputs

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,5 +1,24 @@
 import 'dart:io';
 void main(){
+List <int> num=[10,22,85,64,];
+print(num);
+var num1=[14,25,9,5];
+//List <ver> num1=[14,55,88,];            //problem var
+print(num1);
+
+List name=["Maha,baba,ma,Saim"];
+print(name);
+
+List<dynamic> mixed=[1,"hello",3.25,true];
+print(mixed);
+
+final List<int> nums=[1,2,5];
+print(nums);
+
+const List<double> num4=[1.4,9.8,3.1416];
+print(num4);
+
+
 
 
 }
