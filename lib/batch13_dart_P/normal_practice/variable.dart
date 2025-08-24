23fb88720718af901d82670766c6ed34a0bd9277
Maha6468 <motherma7633@gmail.com>
2025-08-24 12:49:42 +0600

    ..

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,11 +1,5 @@
 import 'dart:io';
 void main(){
-print("Give me 1st number=");
-int num1=int.parse(stdin.readLineSync()!);
-print("Give me 2nd number=");
-int num2=int.parse(stdin.readLineSync()!);
-int sum=num1+num2;
-print("Sum is =$sum");
 
 
 }
