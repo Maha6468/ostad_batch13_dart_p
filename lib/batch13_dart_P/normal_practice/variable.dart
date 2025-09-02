20c373d3b3f62739a6e7c28a1bbea7445d96d18e
Maha6468 <motherma7633@gmail.com>
2025-09-02 17:35:19 +0600

    function

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,23 +1,12 @@
-import 'dart:io';
 void main(){
-List <int> num=[10,22,85,64,];
-print(num);
-var num1=[14,25,9,5];
-//List <ver> num1=[14,55,88,];            //problem var
-print(num1);
+  add(5,5);
+  add(5,7);
+  add(20,5,);
+}
 
-List name=["Maha,baba,ma,Saim"];
-print(name);
-
-List<dynamic> mixed=[1,"hello",3.25,true];
-print(mixed);
-
-final List<int> nums=[1,2,5];
-print(nums);
-
-const List<double> num4=[1.4,9.8,3.1416];
-print(num4);
-
-
-
+add(int a,int b,){
+  var x=a;
+  var y=b;
+  var sum=x+y;
+  print(sum);
 }
