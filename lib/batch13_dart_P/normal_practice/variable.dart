838d968da89ce61ac4342cde763425ba204474ba
Maha6468 <motherma7633@gmail.com>
2025-09-08 00:04:58 +0600

    function

--- lib/batch13_dart_P/normal_practice/variable.dart
+++ lib/batch13_dart_P/normal_practice/variable.dart
@@ -1,17 +1,8 @@
-void check(int a,int b,int c){
-  if(a>=b && a>=c) {
-    print("$a is getter number");
-  }
-  else if(b>=a && b>=c) {
-    print("$b is getter number");
-  }
-  else{
-    print("$c is getter number");
-  }
-}
-
 void main(){
-  check(9,7,6);
-  check(5,35, 26);
-  check(032, 3295, 453);
+  int n=5;
+  int fact=1;
+  for(int i=n;i>=1;i--){
+    fact=fact*i;
+  }
+  print(fact);
 }
