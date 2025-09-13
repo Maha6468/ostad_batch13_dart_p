7ce2de1293bfeb3216e2470824da7e4f5010ffec
Maha6468 <motherma7633@gmail.com>
2025-09-14 02:43:19 +0600

    named constructor

--- lib/oop/constructor/named_constructor.dart
+++ lib/oop/constructor/named_constructor.dart
@@ -1,11 +1,16 @@
 class Person{
   String name;
   int age;
-
   Person(this.name, this.age);
+
+  Person.defult():name="chad",
+  age=4;
 
 }
 void main(){
   var p1=Person("Maha", 24);
   print('My name is ${p1.name} \nI am ${p1.age}');
+  var p2=Person.defult();
+  print("My defult name ${p2.name}");
+  print("My defult age ${p2.age}");
 }
